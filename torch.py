2d4177b867a8a520dab87b335e4aff6551f57d57
Matthew Altenburg <mattaltenburg@gmail.com>
2022-09-19 06:37:48 +1000

    added some helper prints

--- torch.py
+++ torch.py
@@ -3,12 +3,17 @@ def mean_std(data_file_path):
     import torch
     from torchvision import transforms, datasets
     from torch.utils.data import DataLoader
+    import os
 
     try:
-        std = torch.load('std.pt')
-        mean = torch.load('mean.pt')
+        std = torch.load('data/std.pt')
+        mean = torch.load('data/mean.pt')
     except:
         print('No files found building them please wait ...')
+
+        if not os.path.exists('data'):
+            os.makedirs('data')
+
         transformer = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
@@ -28,12 +33,35 @@ def mean_std(data_file_path):
             batch_mean_sqrd += (batch_data ** 2).mean(dim=(0, 2, 3)) #  E[batch_i**2]
 
         mean = batch_mean / len(loader)
-        torch.save(mean, 'mean.pt')
+        torch.save(mean, 'data/mean.pt')
 
         var = (batch_mean_sqrd / len(loader)) - (mean ** 2)
         std = var ** 0.5
-        torch.save(std, 'std.pt')
+        torch.save(std, 'data/std.pt')
     
     print(f'mean & std: {mean}, {std}')    
     
     return mean, std
+
+
+def help_print(imput=None):
+    
+    if imput is None:
+        print("Outline\n\
+'hyper' = hyperparam_help,\n\
+'trans' = transform,\n\
+'device' = cuda or cpu,\n\
+    ")
+    
+    if imput == "hyper":
+        print("hyper = {\n\
+    'learning_rate': .0001, \n\
+    'weight_decay': 0.0, \n\
+    'epochs': 1, \n\
+    'batch_size': 128,\n\
+    'num_workers': 4, \n\
+    }")
+
+    if imput == "device":
+        print("device = 'cuda' if torch.cuda.is_available() else 'cpu'")
+
